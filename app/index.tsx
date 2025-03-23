a8e7ad5252cb11d003c732c09c45f2e3032cc417
Jared Phayer (AVA) <jared.phayer@ovo.com>
2025-03-23 14:30:51 +0000

    chore(setup): install one stack

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100%",
+      }}
+    >
+      <Text>Hello World!</Text>
+    </View>
+  );
+}
---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+// Typed as UserConfig for handy autocomplete
+import type { UserConfig } from 'vite'
+import { one } from 'one/vite'
+
+export default {
+  plugins: [
+    one(),
+  ],
+} satisfies UserConfig
